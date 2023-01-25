686bb39df56ea5e39f7e3e1d29c8682ab01ab5be
Alex Zani <zani.alexandr@gmail.com>
2023-01-25 17:21:33 +0200

    CX-1666: Setup frontend with react to display search results.

---
+++ README.md
@@ -0,0 +1,16 @@
+# VA Search (powered by Algolia)
+## Installation
+
+It is recommended to use the latest Node.js version ~18.13.0 LTS
+
+```bash
+npm install
+npm start
+```
+
+After installation and starting the project you see:
+
+```bash
+Server running at http://localhost:46371
+✨ Built in 817ms
+```
---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import { Hit as AlgoliaHit } from 'instantsearch.js';
+import algoliasearch from 'algoliasearch/lite';
+import React from 'react';
+import {
+  InstantSearch,
+  Highlight,
+  Hits,
+  Pagination,
+  SearchBox,
+} from 'react-instantsearch-hooks-web';
+
+import './App.css';
+
+const searchClient = algoliasearch(
+  'LDJGR5OORW',
+  'baa58cea7b344ad006f7f01492b22bfe'
+);
+
+type HitProps = {
+  hit: AlgoliaHit<{
+    va_title: string;
+    va_url: string;
+    va_body: string;
+  }>;
+};
+
+function Hit({ hit }: HitProps) {
+  return (
+    <>
+      <article>
+        <a href={hit.va_url} target="_blank">
+          <h1>
+            <Highlight attribute="va_title" hit={hit} />
+          </h1>
+        </a>
+        <p>
+          <Highlight attribute="va_body" hit={hit} />
+        </p>
+        <p>{hit.va_url}</p>
+      </article>
+    </>
+  );
+}
+
+export function App() {
+  return (
+    <InstantSearch
+      searchClient={searchClient}
+      indexName="local_drupal"
+      routing={true}
+    >
+      <div className="Container">
+        <SearchBox placeholder="Search" autoFocus />
+        <Hits hitComponent={Hit} />
+        <Pagination className="Pagination" />
+      </div>
+    </InstantSearch>
+  );
+}
